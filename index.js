c70d551fffc0424bfc27180cea92c4aec3310a18
Vladimir Samojlov <v-moi@ya.ru>
2016-02-02 02:10:24 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
-	computerChoice = "rock";
+    computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
+    computerChoice = "paper";
 } else {
-	computerChoice = "scissors";
+    computerChoice = "scissors";
 } console.log("Computer: " + computerChoice);
 
 var compare = function(choice1, choice2) {
@@ -13,24 +13,24 @@ var compare = function(choice1, choice2) {
         return 'The result is a tie!';
     } else if (choice1 === "rock") {
         if (choice2 === "scissors") {
-            return "rock wins";
+            return "you wins";
         }
         else {
-            return "paper wins"    
+            return "you lose"    
         }
     } else if (choice1 === "paper") {
         if (choice2 === "rock") {
-            return "paper wins"
+            return "you wins"
         }
         else {
-            return "scissors wins"
+            return "you lose"
         }
     } else if (choice1 === "scissors") {
         if (choice2 === "paper") {
-            return "scissors wins"
+            return "you lose"
         }
         else {
-            return "rock wins"
+            return "you wins"
         }
     }
 };
