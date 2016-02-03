cb920d572bd68798ac5c2c3d170b07f263150ce1
Vladimir Samojlov <v-moi@ya.ru>
2016-02-03 17:06:08 +0200

    add counter and games reset

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var userChoiсe;
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
+var computerChoice;
+
+function init() {
+  computerChoice = Math.random();
+  if (computerChoice < 0.34) {
     computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+  } else if(computerChoice <= 0.67) {
     computerChoice = "paper";
-} else {
+  } else {
     computerChoice = "scissors";
-} 
+  } 
+}
+init();
+
+var userRes = 0;
+var pcRes = 0;
+
+var reset = function(){
+    $('.win , .lose , .tie').fadeOut();
+    $('img').css({"display":"block"});
+    userChoiсe = undefined;
+    init();
+}
 
 $('.user img').click(function name(){   
     userChoiсe = $(this).data('clk');
@@ -15,33 +30,50 @@ $('.user img').click(function name(){
     $('.computer img').css({"display":"none"});
     $('.computer'+' '+'.'+computerChoice+' '+'img').css({"display":"block"}).animate({"display":"block"},{complete:function(){
         if(userChoiсe === computerChoice) {
-            $('.tie').fadeIn();
+            $('.tie').fadeIn({complete:reset});
         } 
         else if (userChoiсe === "rock") {
             if (computerChoice === "scissors") {
                 $('.win').fadeIn();
+                userRes += 1;
+                $('.user-res').text(userRes);
+                setTimeout(reset, 1300);
             }   
             else {
-                $('.lose').fadeIn();   
+                $('.lose').fadeIn();  
+                pcRes += 1;
+                $('.pc-res').text(pcRes); 
+                setTimeout(reset, 1300);
             }
         }
         else if (userChoiсe === "scissors") {
             if (computerChoice === "paper") {
                 $('.win').fadeIn();
+                userRes += 1;
+                $('.user-res').text(userRes);
+                setTimeout(reset, 1300);
             }   
             else {
                 $('.lose').fadeIn();
+                pcRes += 1;
+                $('.pc-res').text(pcRes);
+                setTimeout(reset, 1300);
             }
         } 
         else if (userChoiсe === "paper") {
             if (computerChoice === "rock") {
                 $('.win').fadeIn();
+                userRes += 1;
+                $('.user-res').text(userRes);
+                setTimeout(reset, 1000);
             }
             else {
                 $('.lose').fadeIn();
+                pcRes += 1;
+                $('.pc-res').text(pcRes);
+                setTimeout(reset, 1000);
             }
         } 
-        $('.button-reset').fadeIn();
     }});
-
+    
 });
